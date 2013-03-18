8de4920362974148b7e2400eba361091b96d88e1
tsukolsky <tsukolsky@gmail.com>
2013-03-18 13:50:24 -0400

    Made the tones work and it is activiated/deactiviate by the button

--- homework4/homework4_tones/main.c
+++ homework4/homework4_tones/main.c
@@ -29,13 +29,13 @@
 #define TA0_BIT (1 << 1)		//0x02 => Bit mask corresponding to TA0
 
 //Define Tone Half Frequencies
-#define C_tone 261/2
-#define D_tone 293/2
-#define E_tone 330/2
-#define F_tone 350/2
-#define G_tone 392/2
-#define A_tone 440/2
-#define B_tone 494/2
+#define C_tone 261
+#define D_tone 293
+#define E_tone 330
+#define F_tone 350
+#define G_tone 392
+#define A_tone 440
+#define B_tone 494
 
 //Define Oscillator frequency
 #define FOSC 1000000
@@ -45,6 +45,7 @@
 /*				Global Variables				*/
 /*==============================================*/
 volatile unsigned int halfPeriod=500;	//1kHz tone.
+volatile unsigned int playingSound=0;	//Whether we are playing a sound at the moment
 
 /*==============================================*/
 /*			Forward Function Declarations		*/
@@ -62,8 +63,8 @@ void main(void){
 	DCOCTL = CALDCO_1MHZ;		//"
 
 	//Initialize timers and buttons
-	initTimerA();
 	initButtonAndLEDS();
+	initTimerA();
 
 	//Enable global interrupts
 	_bis_SR_register(GIE+LPM0_bits);
@@ -76,15 +77,60 @@ void main(void){
 
 void initTimerA(){
 	TA0CTL |= TACLR; 	//reset clock
-	TA0CTL = TASSEL1+ID_0+MC_2;		//clock source is SMCLK, clk divider =1, continuous mode
-	TA0CCTL0 = CCIE;				//turn on output compare interrupt
-	TA0CCR0 = FOSC*(1/(2*C_tone))-1;				//when output is triggered. TO make C_tone, eq => 261Hz= 1MHz/(x) ==> x=1MHz/(2*C_tone)
+	TA0CTL = TASSEL1+ID_0+MC_1;					//clock source is SMCLK, clk divider =1, up mode
+	TA0CCTL0 = CCIE + playingSound;			//turn on output compare interrupt, initially OUTMOD
+	TA0CCR0 = FOSC/C_tone-1;						//when output is triggered. TO make C_tone, eq => 261Hz= 1MHz/(x) ==> x=1MHz/(2*C_tone)
 	
 	//Connect Timer output to pin TA0
 	P1SEL |= TA0_BIT;
 	P1DIR |= TA0_BIT;
 }
 
+/*------------------------------------------*/
+
+void playSong(){
+	volatile static unsigned int state=0;
+	static unsigned int whichRun=0;
+	//State machine
+	switch (state){
+	case 0: {
+		TA0CCR0=FOSC/G_tone-1;
+		break;
+	}
+	case 1: {
+		TACCR0=FOSC/G_tone-1;
+		break;
+	}
+	case 2: {
+		TACCR0=FOSC/G_tone-1;
+		break;
+	}
+	case 3: {
+		TACCR0=FOSC/C_tone-1;
+		state=4;
+		break;
+	}
+	case 4:{
+		TACCR0=FOSC/F_tone-1;
+		state=5;
+		break;
+	}
+	case 5:{
+
+		TACCR0=FOSC/D_tone-1;
+		if (whichRun==0){whichRun++; state=3;}
+		else {state=6;}
+		break;
+	}
+	case 6:{
+		//Get out of this sucker.
+		state=0;
+		whichRun=0;
+		break;
+	}
+	default:{state=0; whichRun=0;break;}
+	}//end switch
+}
 /*------------------------------------------*/
 
 void initButtonAndLEDS(){
@@ -106,25 +152,23 @@ void initButtonAndLEDS(){
 /*==========================================*/
 
 void interrupt buttonISR(){
-	//Debounce variable
-	volatile static unsigned int debounceCounter=0;
 
-	//Make sure we should actually be doing something. ButtonBIt should be low and IFG should be set.
-	if (debounceCounter < 5){debounceCounter++;}
-	else {
-		if (P1IFG & BUTTON_BIT){
-			P1IFG &= ~BUTTON_BIT;	//lower flag
-			TACCTL0 ^= OUTMOD_4;	//toggle OUTMOD between 0 and 4
-			debounceCounter = 0;
-		} else; //do nothing
-	}//end else debounceCounter
+	if (P1IFG&BUTTON_BIT){
+		P1IFG &= ~BUTTON_BIT;	//lower flag
+		playingSound ^= OUTMOD_4;	//toggle OUTMOD between 0 and 4
+		P1OUT ^= RED_LED;
+	} else; //do nothing, not correct occasion
+
 }//end buttonISR handler.
 
 /*------------------------------------------*/
 
 //Timer compare match. Just toggle LEDS.
 void interrupt timerAISR(){
-	P1OUT ^= RED_LED + GREEN_LED;
+	P1OUT ^= GREEN_LED;
+
+	//UPdate control register
+	TACCTL0 = CCIE + playingSound;				//update control register on whether it should be playing a sound.
 }
 
 //Declare interrupt vectors.
